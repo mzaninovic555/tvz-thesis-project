6f3c70408fd7ed3189a607c4f24d99ce83042b9a
Marko Zaninović <mzaninovic555@gmail.com>
2022-07-16 14:16:12 +0200

    Dodan app-dev.properties.

--- src/main/resources/db/migration/V0.0.1__init.sql
+++ src/main/resources/db/migration/V0.0.1__init.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS `publisher` (
     `id` INT NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(255) NOT NULL,
     PRIMARY KEY (`id`)
-) ENGINE = InnoDB;
+);
 -- -----------------------------------------------------
 -- Table `language`
 -- -----------------------------------------------------
@@ -10,7 +10,7 @@ CREATE TABLE IF NOT EXISTS `language` (
     `id` INT NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(20) NOT NULL,
     PRIMARY KEY (`id`)
-) ENGINE = InnoDB;
+);
 -- -----------------------------------------------------
 -- Table `author`
 -- -----------------------------------------------------
@@ -19,7 +19,7 @@ CREATE TABLE IF NOT EXISTS `author` (
     `first_name` VARCHAR(100) NOT NULL,
     `last_name` VARCHAR(100) NOT NULL,
     PRIMARY KEY (`id`)
-) ENGINE = InnoDB;
+);
 -- -----------------------------------------------------
 -- Table `book`
 -- -----------------------------------------------------
@@ -48,7 +48,7 @@ CREATE TABLE IF NOT EXISTS `book` (
     CONSTRAINT `fk_book_publisher1` FOREIGN KEY (`publisher_id`) REFERENCES `publisher` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
     CONSTRAINT `fk_book_language1` FOREIGN KEY (`language_id`) REFERENCES `language` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
     CONSTRAINT `fk_book_author1` FOREIGN KEY (`author_id`) REFERENCES `author` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE = InnoDB;
+);
 -- -----------------------------------------------------
 -- Table `category`
 -- -----------------------------------------------------
@@ -56,7 +56,7 @@ CREATE TABLE IF NOT EXISTS `category` (
     `id` INT NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(45) NOT NULL,
     PRIMARY KEY (`id`)
-) ENGINE = InnoDB;
+);
 -- -----------------------------------------------------
 -- Table `book_category`
 -- -----------------------------------------------------
@@ -67,7 +67,7 @@ CREATE TABLE IF NOT EXISTS `book_category` (
     INDEX `fk_book_category_category1_idx` (`category_id` ASC) VISIBLE,
     CONSTRAINT `fk_book_category_book` FOREIGN KEY (`book_id`) REFERENCES `book` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
     CONSTRAINT `fk_book_category_category1` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE = InnoDB;
+);
 -- -----------------------------------------------------
 -- Table `country`
 -- -----------------------------------------------------
@@ -76,7 +76,7 @@ CREATE TABLE IF NOT EXISTS `country` (
     `name` VARCHAR(45) NOT NULL,
     `country_code` CHAR(2) NOT NULL,
     PRIMARY KEY (`id`)
-) ENGINE = InnoDB;
+);
 -- -----------------------------------------------------
 -- Table `user`
 -- -----------------------------------------------------
@@ -95,7 +95,7 @@ CREATE TABLE IF NOT EXISTS `user` (
     PRIMARY KEY (`id`),
     INDEX `fk_user_country1_idx` (`country_id` ASC) VISIBLE,
     CONSTRAINT `fk_user_country1` FOREIGN KEY (`country_id`) REFERENCES `country` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE = InnoDB;
+);
 -- -----------------------------------------------------
 -- Table `order`
 -- -----------------------------------------------------
@@ -107,7 +107,7 @@ CREATE TABLE IF NOT EXISTS `order` (
     PRIMARY KEY (`id`),
     INDEX `fk_order_user1_idx` (`user_id` ASC) VISIBLE,
     CONSTRAINT `fk_order_user1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE = InnoDB;
+);
 -- -----------------------------------------------------
 -- Table `order_book`
 -- -----------------------------------------------------
@@ -119,7 +119,7 @@ CREATE TABLE IF NOT EXISTS `order_book` (
     INDEX `fk_order_book_book1_idx` (`book_id` ASC) VISIBLE,
     CONSTRAINT `fk_order_book_order1` FOREIGN KEY (`order_id`) REFERENCES `order` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
     CONSTRAINT `fk_order_book_book1` FOREIGN KEY (`book_id`) REFERENCES `book` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE = InnoDB;
+);
 -- -----------------------------------------------------
 -- Table `role`
 -- -----------------------------------------------------
@@ -127,7 +127,7 @@ CREATE TABLE IF NOT EXISTS `role` (
     `id` INT NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(20) NOT NULL,
     PRIMARY KEY (`id`)
-) ENGINE = InnoDB;
+);
 -- -----------------------------------------------------
 -- Table `user_role`
 -- -----------------------------------------------------
@@ -138,7 +138,7 @@ CREATE TABLE IF NOT EXISTS `user_role` (
     INDEX `fk_user_role_user1_idx` (`user_id` ASC) VISIBLE,
     CONSTRAINT `fk_user_role_role1` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
     CONSTRAINT `fk_user_role_user1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE = InnoDB;
+);
 -- -----------------------------------------------------
 -- Table `discount`
 -- -----------------------------------------------------
@@ -151,4 +151,4 @@ CREATE TABLE IF NOT EXISTS `discount` (
     PRIMARY KEY (`id`),
     INDEX `fk_discount_book1_idx` (`book_id` ASC) VISIBLE,
     CONSTRAINT `fk_discount_book1` FOREIGN KEY (`book_id`) REFERENCES `book` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE = InnoDB;
+);
