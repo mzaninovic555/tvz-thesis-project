2996881b5f174d4a7e29b47d7e44ffc4902f5ef2
Marko Zaninovic <mzaninovic@kodilion.hr>
2022-07-25 14:41:40 +0200

    Dodana migracija za kategorije.

--- src/main/resources/db/testdata/V0.0.1.1__init-testdata.sql
+++ src/main/resources/db/testdata/V0.0.1.1__init-testdata.sql
@@ -1,6 +1,6 @@
 -- Category
 insert into category(name)
-values ('Fantasy'), ('Kriminalne'), ('Samopomoć'), ('Povijesna'), ('Romantika'), ('Strane knjige'), ('Social sciences'), ('Sci-fi'), ('Horror'), ('Manga');
+values ('Fantasy'), ('Kriminalne'), ('Samopomoć'), ('Povijesna'), ('Romantika'), ('Strane knjige'), ('Social sciences'), ('Sci-fi'), ('Horror'), ('Manga'), ('Publicistika');
 
 -- Language
 insert into language(name)
@@ -23,6 +23,29 @@ values
     ('162 x 240', 324, 'Tvrdi', 0.30, '9789533236001' ,'Trajni zapisi', 129.00, 'Edward Snowden, čovjek koji je riskirao sve da razotkrije sustav masovnoga nadziranja koji provodi vlada SAD-a, prvi put priča priču svojega života, među ostalim kako je pomogao izgraditi taj sustav i što ga je potaknulo da ga pokuša srušiti. <br>Godine 2013. tada dvadesetdevetogodišnji Edward Snowden šokirao je svijet odmetnuvši se od američke obavještajne zajednice i otkrivši da vlada Sjedinjenih Američkih Država u tajnosti pokušava pronaći način prikupljanja svih telefonskih poziva, SMS-ova i emailova. Rezultat bi bio besprimjeran sustav masovnoga nadzora koji može zadirati u živote svih ljudi na svijetu. <br>Šest godina poslije, Snowdenprvi put opisuje kako je pomogao u nastanku toga sustava i što ga jepotaknulo da ga razotkrije. Od idiličnih predgrađa Washingtona njegova djetinjstva i tajnih zadataka tijekom zaposlenja u CIA-i i NSA-i, Trajni zapisi izvanredna je priča o bistrome mladiću odgojenome na internetu — mladića koji je postao špijun, zviždač i, u progonstvu, savjest interneta. Duhovit, lepršav, strastan i prostodušan, Trajni zapisi važan je dokument o našemu digitalnom dobu predodređen da postane klasikom.', 2029, 3, '4928182892131', 'trajni_zapisi.jpg', '2019-05-17', 4, 1, 5),
     ('146 x 210', 576, 'Tvrdi', 0.72, '9781421553627' ,'Neon Genesis Evangelion 3-in-1, vol.3', 159.00, 'Big robot hmmm', 2013, 1, '4928182892131', 'evangelion_vol_3.jpg', '2021-02-04', 1, 2, 1),
     ('135 x 205', 600, 'Meki', 0.60, '9789531318341' ,'Dina', 149.00, 'HMMMM', 2021, 1, '4928182892131', 'dina.jpg', '2020-09-24', 2, 1, 1);
+
+insert into book_category(book_id, category_id)
+values
+    (1, 6),
+    (1, 9),
+
+    (2, 4),
+    (2, 6),
+    (2, 11),
+
+    (3, 1),
+    (3, 6),
+
+    (4, 4),
+    (4, 11),
+
+    (5, 10),
+    (5, 1),
+    (5, 6),
+    (5, 8),
+
+    (6, 1),
+    (6, 6);
 
 insert into discount(discount_price, started_at, ends_at, book_id)
 values
