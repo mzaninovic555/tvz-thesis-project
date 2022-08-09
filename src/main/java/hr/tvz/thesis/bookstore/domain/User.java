cefffeb761c96369586db1375de025b3b6840fe9
Marko Zaninović <mzaninovic555@gmail.com>
2022-08-09 18:57:53 +0200

    Dodane forme za unos svega novog

--- src/main/java/hr/tvz/thesis/bookstore/domain/User.java
+++ src/main/java/hr/tvz/thesis/bookstore/domain/User.java
@@ -17,7 +17,6 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
